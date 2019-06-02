bd73e08b75f4d63b35ab77602f1a23b2b72f9523
Rob Clegg <roberto198@github.com>
2019-06-02 12:07:21 +0100

    Editing readme

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # NCNews
 
-This is a React web aplication designed to serve new articles and ther associated comments. it is hosted on netlify and it works with the API that i have built and hosted with heroku.
+This is a React web aplication designed to serve new articles and ther associated comments. Designed primarily for mobile it is also responsive and will work on desktop. It is hosted on netlify and it works with the API that i have built and hosted with heroku.
+
+- Hosted Site: [https://robs-news-app.netlify.com/](https://robs-news-app.netlify.com/)
+
+- Hosted API: [https://northcodersapinews.herokuapp.com/api](https://northcodersapinews.herokuapp.com/api)
+
+- Back End Repo: [https://github.com/Roberto198/nc_news](https://github.com/Roberto198/nc_news)
 
 ## Getting Started
 
@@ -8,18 +14,32 @@ To adapt develop or test this project for this repository on github and clone to
 
 ### Prerequisites
 
-The software runs on node and react. Type npm -i to install all dependencies.
+The software runs on the node runtime envronment. It is built with the React.JS library.
 
 ### Installing
 
-Once you have cloned and installed dependencies you can run the API server. Type npm start into your terminal and point your internet browser to the appropriate local host port shown in your terminal.
+To clone and install this repo:
+
+```
+git clone https://github.com/Roberto198/ncnewsfrontend
+```
+
+```
+npm i
+```
+
+Once you have cloned and installed dependencies you can run the API server. Type this into your terminal and point your internet browser to the appropriate local host port shown in your terminal:
+
+```
+npm start
+```
 
 ## Built With
 
--   [Node](https://nodejs.org/en/) - The JS runtime environment.
--   [React.js](https://reactjs.org/) - A JavaScript library for building user interfaces.
--   [MaterialUI](https://material-ui.com/) - React components for faster and easier web development. Build your own design system, or start with Material Design.
+- [Node](https://nodejs.org/en/) - The JS runtime environment.
+- [React.js](https://reactjs.org/) - A JavaScript library for building user interfaces.
+- [MaterialUI](https://material-ui.com/) - React components for faster and easier web development. Build your own design system, or start with Material Design.
 
 ## Authors
 
--   **Robert Clegg** - Architect - [Roberto198](https://github.com/Roberto198)
+- **Robert Clegg** - Architect - [Roberto198](https://github.com/Roberto198)
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,132 +1,140 @@
-import React from 'react';
-import CommentsContainer from './CommentsContainer';
-import { axiosGetAllArticles, axiosRemove, axiosIncVotes } from '../api/axios';
-import { Link } from '@reach/router';
-import VoteButtons from './VoteButtons';
-import { Paper, Typography, withStyles } from '@material-ui/core';
+import React from "react";
+import CommentsContainer from "./CommentsContainer";
+import { axiosGetAllArticles, axiosRemove, axiosIncVotes } from "../api/axios";
+import { Link } from "@reach/router";
+import VoteButtons from "./VoteButtons";
+import { Paper, Typography, withStyles } from "@material-ui/core";
 
 const styles = {
-	div: {
-		padding: '10vh 5vh',
-		margin: '5vw',
-		maxWidth: '900px',
-	},
-	body: {
-		textIndent: '20px',
-		initialLetter: '2',
-		maxWidth: '900px',
-	},
+  div: {
+    padding: "10vh 5vh",
+    margin: "5vw",
+    maxWidth: "900px"
+  },
+  body: {
+    textIndent: "20px",
+    initialLetter: "2",
+    maxWidth: "900px"
+  },
 
-	votes: {
-		textAlign: 'center',
-		paddingTop: '10px',
-		margin: '5vw',
-		maxWidth: '900px',
-	},
-	detail: {
-		display: 'flex',
-		justifyContent: 'center',
-		margin: '5vw',
-		padding: '10px',
-		maxWidth: '900px',
-	},
+  votes: {
+    textAlign: "center",
+    paddingTop: "10px",
+    margin: "5vw",
+    maxWidth: "900px"
+  },
+  detail: {
+    display: "flex",
+    justifyContent: "center",
+    margin: "5vw",
+    padding: "10px",
+    maxWidth: "900px"
+  }
 };
 
 class Article extends React.Component {
-	state = {
-		article: undefined,
-		vote: 0,
-		isLoading: true,
-		err: null,
-	};
+  state = {
+    article: null,
+    vote: 0,
+    isLoading: true,
+    err: null
+  };
 
-	componentDidMount() {
-		axiosGetAllArticles({}, this.props.id)
-			.then(({ data: { article } }) => {
-				this.setState({ article, isLoading: false });
-			})
-			.catch(({ response: { data } }) => {
-				this.setState({ err: data, isLoading: false });
-			});
-	}
+  componentDidMount() {
+    axiosGetAllArticles({}, this.props.id)
+      .then(({ data: { article } }) => {
+        this.setState({ article, isLoading: false });
+      })
+      .catch(({ response: { data } }) => {
+        this.setState({ err: data, isLoading: false });
+      });
+  }
 
-	render() {
-		if (this.state.isLoading) {
-			return <h3> Loading...</h3>;
-		}
-		if (this.state.err) {
-			return (
-				<div className="err">
-					{' '}
-					<h3> Error: {this.state.err.msg}</h3>
-				</div>
-			);
-		} else {
-			const { title, body, votes, topic, author, comment_count, created_at } = this.state.article;
-			const { vote } = this.state;
-			const { classes, loggedInUser, id } = this.props;
+  render() {
+    if (this.state.isLoading) {
+      return <h3> Loading...</h3>;
+    }
+    if (this.state.err) {
+      return (
+        <div className='err'>
+          {" "}
+          <h3> Error: {this.state.err.msg}</h3>
+        </div>
+      );
+    } else {
+      const {
+        title,
+        body,
+        votes,
+        topic,
+        author,
+        comment_count,
+        created_at
+      } = this.state.article;
+      const { vote } = this.state;
+      const { classes, loggedInUser, id } = this.props;
 
-			return (
-				<div className={classes.wrapper}>
-					<div>
-						<Paper className={classes.div}>
-							<Typography variant="h4" p="20px" gutterBottom>
-								{title}
-							</Typography>
-							<Typography variant="subtitle2">By {author} </Typography>
-						</Paper>
-						<Paper className={classes.div}>
-							<div>
-								<Typography className={classes.body} variant="h6" gutterBottom>
-									{body}
-								</Typography>
-							</div>
-						</Paper>
-						<Paper className={classes.div}>
-							<Typography>
-								By: <Link to={`/users/${author}`}>{author}</Link>
-								<br />
-								Topic: <Link to={`/topics/${topic}`}>{topic}</Link>
-								<br />
-								Votes: {votes + vote}
-								<br />
-								Comments: {comment_count}
-								<br />
-								Written: {new Date(created_at).toDateString()}
-							</Typography>
-							{this.props.loggedInUser && (
-								<div className={classes.votes}>
-									<VoteButtons
-										loggedInUser={loggedInUser}
-										voteFunc={this.vote}
-										remove={this.remove}
-										voteValue={vote}
-										author={author}
-										id={id}
-										media="articles"
-									/>
-								</div>
-							)}
-						</Paper>
-					</div>{' '}
-					<Paper className={classes.detail}>
-						<CommentsContainer article={id} loggedInUser={loggedInUser} />
-					</Paper>
-				</div>
-			);
-		}
-	}
+      return (
+        <div className={classes.wrapper}>
+          <div>
+            <Paper className={classes.div}>
+              <Typography variant='h4' p='20px' gutterBottom>
+                {title}
+              </Typography>
+              <Typography variant='subtitle2'>By {author} </Typography>
+            </Paper>
+            <Paper className={classes.div}>
+              <div>
+                <Typography className={classes.body} variant='h6' gutterBottom>
+                  {body}
+                </Typography>
+              </div>
+            </Paper>
+            <Paper className={classes.div}>
+              <Typography>
+                By: <Link to={`/users/${author}`}>{author}</Link>
+                <br />
+                Topic: <Link to={`/topics/${topic}`}>{topic}</Link>
+                <br />
+                Votes: {votes + vote}
+                <br />
+                Comments: {comment_count}
+                <br />
+                Written: {new Date(created_at).toDateString()}
+              </Typography>
+              {this.props.loggedInUser && (
+                <div className={classes.votes}>
+                  <VoteButtons
+                    loggedInUser={loggedInUser}
+                    voteFunc={this.vote}
+                    remove={this.remove}
+                    voteValue={vote}
+                    author={author}
+                    id={id}
+                    media='articles'
+                  />
+                </div>
+              )}
+            </Paper>
+          </div>{" "}
+          <Paper className={classes.detail}>
+            <CommentsContainer article={id} loggedInUser={loggedInUser} />
+          </Paper>
+        </div>
+      );
+    }
+  }
 
-	vote = (direction, id) => {
-		axiosIncVotes(direction, 'articles', id);
-		this.setState(prevState => {
-			return { vote: prevState.vote + direction };
-		});
-	};
+  vote = (direction, id) => {
+    axiosIncVotes(direction, "articles", id);
+    this.setState(prevState => {
+      return { vote: prevState.vote + direction };
+    });
+  };
 
-	remove = (media, id) => {
-		axiosRemove(media, id);
-	};
+  remove = (media, id) => {
+    axiosRemove(media, id);
+  };
 }
 
 export default withStyles(styles)(Article);
